075a02f3d68667120160380600ff12d127d2e682
M4NGATA <mangatasv@pm.me>
2024-03-04 15:10:20 +0300

    update babylon menu

--- Nodes/active/Babylon/main.sh
+++ Nodes/active/Babylon/main.sh
@@ -1,61 +1,44 @@
-#!/bin/bash
-	clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Common/theme.sh) && printlogo
+#!/bin/bash -e
+
+# Function to print main menu
 mainmenu() { 
-		echo "$(printBYellow ' BABYLON')"
-		echo "$(printBGreen ' 1 ')Управление"
-		echo "$(printBGreen ' 2 ')Обновить"
-		echo "$(printBGreen ' 3 ')Установить"
-		echo "$(printBGreen ' 4 ')Удалить"
-		echo "$(printBGreen ' 5 ')Инфо"
-		echo ' ---------'
-		echo "$(printBBlue '  0 ')Назад"
-		echo "$(printBRed ' 10 ')Выход"
-		echo ' ---------'
-		echo -ne "$(printBGreen ' Ввод')$(printGreenBlink ': ')"
-#	Свойства меню
-read -r ans
-	case $ans in
-		1)
-		source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Nodes/active/Babylon/control.sh)
-		;;
-		2)
-		clear && printlogo && mainmenu
-		#source <(curl -s https://raw.githubusercontent.com/M4NGATA/X-Script/main/Nodes/active/Babylon/update.sh)
-		;;
-		3)
-		source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Nodes/active/Babylon/install.sh)
-		;;
-		4)
-		source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Nodes/active/Babylon/delet.sh)
-		;;
-		5)
-		info
-		;;
-		0)
-		source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Menu/menu_nodes.sh)
-		;;
-		10)
-		echo $(printBCyan '	"Bye bye."') && exit
-		;;
-		*)
-		clear && printlogo && echo "$(printBRed ' Неверный запрос!')" && mainmenu
-		;;
-	esac
+clear && echo -e "\e[1;33m
+╔╗ ╔═╗╔╗ ╦ ╦╦  ╔═╗╔╗╔
+╠╩╗╠═╣╠╩╗╚╦╝║  ║ ║║║║
+╚═╝╩ ╩╚═╝ ╩ ╩═╝╚═╝╝╚╝\e[0m"
+    echo " 1. Manage"
+    echo " 2. Install"
+    echo " 3. Delete"
+    echo " 5. Info"
+    echo " ---------"
+    echo " 0. Back"
+    echo " 10. Exit"
+    echo " ---------"
+    echo -ne "Input: "
+    read -r ans
+
+    case $ans in
+        1) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Nodes/active/Babylon/control.sh) ;;
+        2) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Nodes/active/Babylon/install.sh) ;;
+        3) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Nodes/active/Babylon/delete.sh) ;;
+        4) info ;;
+        0) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Menu/menu_nodes.sh) ;;
+        10) echo "Bye bye." && exit ;;
+        *) clear && echo "Invalid input!" && mainmenu ;;
+    esac
 }
 
- 
-
+# Function to display information about Babylon
 info(){
-	clear && printlogo
-	echo "$(printBYellow ' -------------------------------------')"
-	echo Babylon — это новый проект Cosmos, цель которого — использовать безопасность
-	echo Биткойна для повышения безопасности зон Cosmos и других цепочек PoS. Команда
-	echo Babylon утверждает, что они являются первой площадкой, предлагающей биткойн 
-	echo миру PoS. Протокол стейкинга биткойна от Babylon предоставляет способ внедрения
-	echo биткойна в качестве стейкинг-актива для цепей PoS.
-	echo "$(printBYellow ' -------------------------------------')"
-	mainmenu
+    clear
+    echo "-------------------------------------"
+    echo "Babylon is a new Cosmos project aimed at using Bitcoin security"
+    echo "to enhance the security of Cosmos zones and other PoS chains. The"
+    echo "Babylon team claims to be the first platform to offer Bitcoin"
+    echo "to the PoS world. The Babylon Bitcoin staking protocol provides"
+    echo "a way to integrate Bitcoin as a staking asset for PoS chains."
+    echo "-------------------------------------"
+    mainmenu
 }
-
 
 mainmenu
