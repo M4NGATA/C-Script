0dd91fb26d7fd79026dee57eb741077d153b2653
M4NGATA <mangatasv@pm.me>
2024-03-22 12:58:30 +0300

    update

--- Projects/Taiko/main.sh
+++ Projects/Taiko/main.sh
@@ -17,11 +17,11 @@ echo -e "\e[1;35m
     read -r ans
 
     case $ans in
-        1) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/control.sh) ;;
-        2) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/install.sh) ;;
-        3) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/delete.sh) ;;
-        4) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/update.sh) ;;
-        0) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Menu/menu_nodes.sh) ;;
+        1) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/control.sh) ;;
+        2) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/install.sh) ;;
+        4) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/delete.sh) ;;
+        3) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/update.sh) ;;
+        0) clear && source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Menu/menu_nodes.sh) ;;
         10) echo "Bye bye." && exit ;;
         *) clear && echo "Invalid input!" && mainmenu ;;
     esac
