4d57a73e93bb5115c239ff8c10b79a9ef74a0974
M4NGATA <mangatasv@pm.me>
2024-03-22 12:32:01 +0300

    Update main.sh

--- Projects/Taiko/main.sh
+++ Projects/Taiko/main.sh
@@ -18,7 +18,7 @@ echo -e "\e[1;35m
     case $ans in
         1) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/install.sh) ;;
         2) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/delete.sh) ;;
-        3) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/update.sh)
+        3) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Projects/Taiko/update.sh) ;;
         0) source <(curl -s https://raw.githubusercontent.com/M4NGATA/C-Script/main/Menu/menu_nodes.sh) ;;
         10) echo "Bye bye." && exit ;;
         *) clear && echo "Invalid input!" && mainmenu ;;
